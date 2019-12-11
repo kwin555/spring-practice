1d35c6ce2f9a958e42454a1cd5cd6b1f540ce3c5
kevin <kev.ftw595@gmail.com>
2019-12-10 21:47:03 -0600

    intro to spring boot dependency injection

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,24 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearchImpl {
+    //Sorting an array
+    //Search the array
+    //return the result
+    @Autowired
+    private SortingAlgorithm sortingAlgorithm;
+
+    public BinarySearchImpl(SortingAlgorithm sortingAlgorithm) {
+        this.sortingAlgorithm = sortingAlgorithm;
+    }
+
+
+    public int binarySearch(int[] numbers, int numberToSearch) {
+        int [] sortedNums = sortingAlgorithm.sort(numbers);
+        System.out.println(sortingAlgorithm);
+        return 3;
+    }
+}
---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SortingAlgorithm.java
@@ -0,0 +1,5 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public interface SortingAlgorithm {
+    int [] sort(int [] numbers);
+}

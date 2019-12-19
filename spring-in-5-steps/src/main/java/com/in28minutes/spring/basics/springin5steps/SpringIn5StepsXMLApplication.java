ccabfe89e356a2c7c61299751a9049eec3fb68e2
kevin <kev.ftw595@gmail.com>
2019-12-18 18:04:20 -0600

    annotcation with beans

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLApplication.java
@@ -2,29 +2,32 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
 import com.in28minutes.spring.basics.springin5steps.xml.XMLPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan("com.in28minutes.spring.basics.springin5steps")
 public class SpringIn5StepsXMLApplication {
 
 	//What are the beans?
 	//What are the dependencies ofa bean?
 	//Where to Search for a bean?
 
+	private static Logger logger = LoggerFactory.getLogger(SpringIn5StepsXMLApplication.class);
+
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext applicationContext =
-			new ClassPathXmlApplicationContext("applicationContext.xml");
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+			"applicationContext.xml")) {
 
-		XMLPersonDAO personDAO =
-			applicationContext.getBean(XMLPersonDAO.class);
-
-		System.out.println(personDAO);
-		System.out.println(personDAO.getXMLJdbcConnection());
+			logger.info("Beans loaded -> {}",
+				(Object) applicationContext.getBeanDefinitionNames());
+			XMLPersonDAO personDao = applicationContext.getBean(XMLPersonDAO.class);
+			System.out.println(personDao);
+			System.out.println(personDao.getXMLJdbcConnection());
+		}
 	}
 
 }
